1ba0689c664853a8dc780e418ff4da5c7373c5a3
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-02-09 13:02:28 +0000

    bookers2-8

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2023_02_08_140331) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
