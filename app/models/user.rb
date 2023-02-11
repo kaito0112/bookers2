b65fc756978b7517efd95bb1a186c56f0de6816b
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-02-11 10:35:39 +0000

    bookers2-16

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
 
   has_one_attached :image
 
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('')
-      profile_image.attach(io: File.open(file_path), filename: '', content_type: '')
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
