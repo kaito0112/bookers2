f167828c37d2b0391740139effaca03d5abe1f55
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-02-11 13:17:20 +0000

    bookers2-17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
@@ -8,5 +9,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+    
 end
