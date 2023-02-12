c5a107895a1b9dd089019bccd16fd5220b99c825
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-02-12 07:48:55 +0000

    bookers2-21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-     @book.user_id = current_user.id
+    @book.user_id = current_user.id
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -24,6 +25,7 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
     redirect_to user_path(user.id)
   end
 
